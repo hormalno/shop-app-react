2bf75b7a09e6294b30a540042a60d194f32f8276
hormalno <ivanov.yasen@gmail.com>
2021-01-18 12:34:10 +0200

    restructuring 18.01.2021

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Header from './Components/Header';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about-us">
+            {/* <AboutUs /> */}
+        </Route>
+        <Route path="/contact-us">
+            {/* <ContactUs /> */}
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,50 +1,41 @@
-import { Navbar } from "react-bootstrap";
-import { Container } from "react-bootstrap/lib/Tab";
+import { Form, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css"
 
 function Header (){
-        return (
-            <div>
+    return (
+        <div>
             <Navbar>
-                <Container>
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#collapse-1" aria-expanded="false">
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        <a className="navbar-brand" href="/"><span className="fa fa-home"></span><span className="link"> Home</span></a>
-                    </div>
-                    <div className="navbar-collapse collapse" id="collapse-1">
-                        <ul className="nav navbar-nav">
-                            <li className="dropdown">
-                                <a href="/" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                    <span className="fa fa-tags"></span><span className="link"> Products</span> <span className="fa fa-caret-down"></span></a>
-                                <ul className="dropdown-menu">
-                                    <li><a href="/"><span className="fa fa-tag"></span> Catalogue 1</a></li>
-                                    <li><a href="/"><span className="fa fa-tag"></span> Catalogue 2</a></li>
-                                </ul>
-                            </li>
-                            <li className="dropdown">
-                                <a href="/" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                    <span className="fa fa-gears"></span><span className="link"> Services</span> <span className="fa fa-caret-down"></span></a>
-                                <ul className="dropdown-menu">
-                                    <li><a href="/"><span className="fa fa-gear"></span> Service 1</a></li>
-                                    <li><a href="/"><span className="fa fa-gear"></span> Service 2</a></li>
-                                    <li><a href="/"><span className="fa fa-gear"></span> Service 3</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="/"><span className="fa fa-info-circle"></span><span className="link"> About</span></a></li>
-                            <li><a href="/"><span className="fa fa-phone"></span><span className="link"> Contact</span></a></li>
+                <Navbar.Brand href="/"><span className="fa fa-home"></span><span className="link"> Home</span></Navbar.Brand>
+                {/* <div className="navbar-collapse collapse" id="collapse-1"> */}
+                <Nav>
+                    <NavDropdown as="li">
+                        <NavDropdown.Toggle href="/" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            <span className="fa fa-tags"></span><span className="link"> Products</span> <span className="fa fa-caret-down"></span></NavDropdown.Toggle>
+                        <NavDropdown.Menu className="dropdown-menu">
+                            <NavDropdown.Item><NavLink to="/"><span className="fa fa-tag"></span> Catalogue 1</NavLink></NavDropdown.Item>
+                            <NavDropdown.Item><NavLink to="/"><span className="fa fa-tag"></span> Catalogue 2</NavLink></NavDropdown.Item>
+                        </NavDropdown.Menu>
+                    </NavDropdown>
+                    <li className="dropdown">
+                        <a href="/" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            <span className="fa fa-gears"></span><span className="link"> Services</span> <span className="fa fa-caret-down"></span></a>
+                        <ul className="dropdown-menu">
+                            <li><NavLink to="/"><span className="fa fa-gear"></span> Service 1</NavLink></li>
+                            <li><NavLink to="/"><span className="fa fa-gear"></span> Service 2</NavLink></li>
+                            <li><NavLink to="/"><span className="fa fa-gear"></span> Service 3</NavLink></li>
                         </ul>
-                        <form className="navbar-form navbar-right">
-                            <div className="form-group">
-                                <input type="text" className="form-control" placeholder="Search" />
-                            </div>
-                            <button type="submit" className="btn"><span className="fa fa-search"></span></button>
-                        </form>
-                    </div>
-                </Container>
+                    </li>
+                    <Nav.Item as="li"><NavLink to="/"><span className="fa fa-info-circle"></span><span className="link"> About</span></NavLink></Nav.Item>
+                    <Nav.Item as="li"><NavLink to="/"><span className="fa fa-phone"></span><span className="link"> Contact</span></NavLink></Nav.Item>
+                </Nav>
+                <Form inline>
+                    <Form.Group>
+                        <input type="text" className="form-control" placeholder="Search" />
+                    </Form.Group>
+                    <button type="submit" className="btn"><span className="fa fa-search"></span></button>
+                </Form>
+                {/* </div> */}
             </Navbar>
         </div>
     )
