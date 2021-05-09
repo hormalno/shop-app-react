1c1b94681ff46b7dd6ceb3b2f2b3658df81055f8
hormalno <ivanov.yasen@gmail.com>
2021-05-09 22:23:00 +0300

    rework infobox

--- src/Components/main/InfoBox.js
+++ src/Components/main/InfoBox.js
@@ -1,16 +1,95 @@
 import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
+import Carousel from 'react-bootstrap/Carousel'
+import image from './infobox.jpg'
 
 class InfoBox extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            slideIndex: 0,
+            cards: [
+                {title: 'a', imgSrc: 'http://placehold.it/380?text=1'},
+                {title: 'b', imgSrc: 'http://placehold.it/380?text=2'},
+                {title: 'c', imgSrc: 'http://placehold.it/380?text=3'},
+                {title: 'd', imgSrc: 'http://placehold.it/380?text=4'}
+            ],
+            cards2: [
+                {title: 'e', imgSrc: 'http://placehold.it/380?text=4'},
+                {title: 'f', imgSrc: 'http://placehold.it/380?text=4'},
+                {title: 'g', imgSrc: 'http://placehold.it/380?text=4'},
+                {title: 'h', imgSrc: 'http://placehold.it/380?text=4'}
+            ]
+        };
+
+        this.handleSelect = this.handleSelect.bind(this);
+    }
+
+    handleSelect(selectedIndex, e) {
+        this.setState({
+            slideIndex: selectedIndex
+        })
+        console.log(selectedIndex)
+    };
+
+    renderCard(item,index) {
+        return (
+            <div className="col-md-3" style={{float:'left'}}>
+            <Card style={{width: '16rem'}} key={index}>
+                <Card.Img variant="top" src={image} />
+                <Card.ImgOverlay><a href='/' className='btn btn-primary'>Quick view</a></Card.ImgOverlay>
+                <Card.Body>
+                    <Card.Title>Card {item.title}</Card.Title>
+                    <Card.Text>
+                        Living room group
+                    </Card.Text>
+                    <Card.Text>
+                        <small className="text-muted">5000 EUR</small>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            </div>
+        )
+    }
+
+    renderCards() {
+        const cards = this.state.cards.map((item,index) => {
+            return this.renderCard(item,index);
+        })
+
+        const cards2 = this.state.cards2.map((item,index) => {
+            return this.renderCard(item,index);
+        })
+
+        return (<div className="carousel-inner" role="listbox">
+                    <Carousel.Item active>
+                        {cards}
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        {cards2}
+                    </Carousel.Item>
+                </div>)
+            }
+
     render() {
         return (
-            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 mb30">
-                <div class="main_box">
-                    <div class="box_1"><a href="#">asd</a></div>
-                    <div class="desc">
-                        <h5><a href="#">Collection Title 4</a></h5>
+            <Container className="text-center my-3">
+                <h2 className="font-weight-light">Bootstrap 4 - Multi Item Carousel</h2>
+                <Carousel id="multi-item" className="carousel slide carousel-multi-item" data-ride="carousel">
+                    <div className="controls-top">
+                        <a className="btn-floating" href="#multi-item" data-slide="prev"><i className="fas fa-chevron-left"></i>Prev</a>
+                        <a className="btn-floating" href="#multi-item" data-slide="next">Next<i className="fas fa-chevron-right"></i></a>
                     </div>
-                </div>
-            </div>
+                    <ol className="carousel-indicators">
+                        <li data-target="#multi-item" data-slide-to="0" className="active"></li>
+                        <li data-target="#multi-item" data-slide-to="1"></li>
+                    </ol>
+                    {this.renderCards()}
+                </Carousel>
+            </Container>
         );
     }
 }
--- src/Components/main/Main.js
+++ src/Components/main/Main.js
@@ -41,58 +41,48 @@ class Main extends Component {
 
        return (
     <div id="content" >
-        <div id="shopify-section-slideshow" class="shopify-section">
+        <div>
             <LazyLoadedCarousel slides={imageCarousel.images}/>
         </div>
-        <div id="shopify-section-featured-collections" class="shopify-section">
-            <div class="container" data-section-id="featured-collections" data-section-type="featured-collections-section">
-                <div class="h_row_content home_collection section-featured-collections">
-                    <div class="clearfix">
-                        <h3>Collection list</h3>
-                    </div>
-                    <div class="catalog_c mt30">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="product_c">
-                                    <div class="row collection-grid view-grid fadeInUp">
-                                        <InfoBox />
-                                        <InfoBox />
-                                        <InfoBox />
-                                        <InfoBox />
-                                    </div>
+
+        <div className="container text-center my-3">
+            <h2 className="font-weight-light">Bootstrap 4 - Multi Item Carousel</h2>
+            <div className="row mx-auto my-auto">
+                <div id="recipeCarousel" className="carousel slide w-100" data-ride="carousel">
+                <a className="carousel-control-prev w-auto"  role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next w-auto" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a> 
+                    <div className="carousel-inner w-100" role="listbox">
+                        <div className="carousel-item active">
+                            <div className="col-md-4">
+                                <div className="card card-body">
+                                    <img className="img-fluid" src="http://placehold.it/380?text=1" alt='text1'/>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div> 
-        </div>
-        <div id="shopify-section-video" class="shopify-section home-video-section">
-            <div data-section-id="video" data-section-type="video-section" class="mb30  video-section-video" data-videotype="youtube" data-videodisplay="autoplay_banner">
-                <div class=" hero-video-wrapper home-video section-video hero--medium" id="hero-video-video">
-                    <div class="vidcover"></div> 
-                    <iframe class="fullvid" id="video-iframe-mobile-video" data-video-id="_9VUPq3SxOc"  frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" width="1280" height="720" src="https:www.youtube.com/embed/_9VUPq3SxOc?autohide=0&amp;branding=0&amp;cc_load_policy=0&amp;controls=0&amp;fs=0&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;rel=0&amp;showinfo=0&amp;loop=1&amp;wmode=opaque&amp;videoId=_9VUPq3SxOc&amp;type=youtube&amp;mute=1&amp;autoplay=1&amp;attemptedToPlay=false&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fhormalno.myshopify.com&amp;widgetid=1"></iframe>
-                    <div class="text-center hero-video-caption hero-video-caption-video container">
-                        <h3 class="mb30 video-title video-title-video ">Featured video</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div id="shopify-section-1611833871fbd2bc70" class="shopify-section">
-            <div class="container" data-section-id="1611833871fbd2bc70" data-section-type="featured-collections-section">
-                <div class="h_row_content home_collection section-1611833871fbd2bc70">
-                    <div class="clearfix">
-                        <h3>Collection list</h3>
-                    </div>
-                    <div class="catalog_c mt30">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="product_c">
-                                    <div class="row collection-grid view-grid fadeInUp">
-                                        <InfoBox />
-                                        <InfoBox />
-                                        <InfoBox />
-                                    </div>
+                        <div className="carousel-item">
+                            <div className="col-md-4">
+                                <div className="card card-body">
+                                    <img className="img-fluid" src="http://placehold.it/380?text=2" alt='text2' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <div className="col-md-4">
+                                <div className="card card-body">
+                                    <img className="img-fluid" src="http://placehold.it/380?text=3" alt='text3' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <div className="col-md-4">
+                                <div className="card card-body">
+                                    <img className="img-fluid" src="http://placehold.it/380?text=4" alt='text4' />
                                 </div>
                             </div>
                         </div>
@@ -100,11 +90,31 @@ class Main extends Component {
                 </div>
             </div>
         </div>
-        <div id="shopify-section-16118342097bc43a8a" class="shopify-section">
-            <div class="home-cta-bg mt-30   mb30 section-16118342097bc43a8a" data-section-id="16118342097bc43a8a" data-section-type="text-section">
-                <div class="cta-overlay">
-                    <div class="container">
-                        <div class="h_row_content h_row_3">
+       
+        
+        <div id="shopify-section-video" className="shopify-section home-video-section">
+            <div data-section-id="video" data-section-type="video-section" className="mb30  video-section-video" data-videotype="youtube" data-videodisplay="autoplay_banner">
+                <div className=" hero-video-wrapper home-video section-video hero--medium" id="hero-video-video">
+                    <div className="vidcover"></div> 
+                    <iframe className="fullvid" id="video-iframe-mobile-video" data-video-id="_9VUPq3SxOc"  frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" width="1280" height="720" src="https:www.youtube.com/embed/_9VUPq3SxOc?autohide=0&amp;branding=0&amp;cc_load_policy=0&amp;controls=0&amp;fs=0&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;rel=0&amp;showinfo=0&amp;loop=1&amp;wmode=opaque&amp;videoId=_9VUPq3SxOc&amp;type=youtube&amp;mute=1&amp;autoplay=1&amp;attemptedToPlay=false&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fhormalno.myshopify.com&amp;widgetid=1"></iframe>
+                    <div className="text-center hero-video-caption hero-video-caption-video container">
+                        <h3 className="mb30 video-title video-title-video ">Featured video</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div className="clearfix">
+            <h3>Collection list</h3>
+        </div>
+        
+        <InfoBox />
+                
+        <div id="shopify-section-16118342097bc43a8a" className="shopify-section">
+            <div className="home-cta-bg mt-30   mb30 section-16118342097bc43a8a" data-section-id="16118342097bc43a8a" data-section-type="text-section">
+                <div className="cta-overlay">
+                    <div className="container">
+                        <div className="h_row_content h_row_3">
                             <h2><p>Large Call to Action Area</p></h2>
                             <p>Add a background image or color to this section and create some keyword-rich text about your store here. Great for SEO! You can customize this section with any heading, text, link, and image you want in Theme Settings.</p>
                         </div>
