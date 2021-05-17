f528b3a3e77a9c85ef006e7f403fe4ccece21d9c
hormalno <ivanov.yasen@gmail.com>
2021-05-17 17:37:50 +0300

    rework swiper

--- src/Components/main/Carousel2.js
+++ src/Components/main/Carousel2.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Image from 'react-bootstrap/Image'
 import "./Carousel2.css"
+import styled from 'styled-components';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import image01 from './image001.jpg'
+import image01 from './../../../src/Assets/image001.jpg'
+import image02 from './../../../src/Assets/image002.jpg'
+import image03 from './../../../src/Assets/image003.jpg'
 
 // import Swiper core and required modules
 import SwiperCore, {Navigation} from 'swiper/core';
@@ -13,19 +17,52 @@ SwiperCore.use([Navigation]);
 
 function Carousel2 () {
 
-    const params = { 
-        navigation: true,   
-        className:"CarouselSwiper"
-    }
-
+    const props = [        
+        {
+            imageSource: {image01},
+            imageAlt: 'first slide',
+            imageHeader: 'First Slide',
+            imageText: 'asd'
+        },
+        {
+            imageSource: image02,
+            imageAlt: 'second slide',
+            imageHeader: 'Second Slide',
+            imageText: 'zxc'
+        },
+        {
+            imageSource: image03,
+            imageAlt: 'third slide',
+            imageHeader: 'Third Slide',
+            imageText: 'zxc'
+        }
+    ]
+   
+    const CarouselImage = styled.img `
+        width: 100%; 
+        max-height: 800px;
+        height: 'auto';
+    `
+    function renderSlides(props) {
+        const slides = props.map( (slide, index) => {
+            return (<SwiperSlide key={index}>
+                        <CarouselImage  src={image01} fluid />
+                        <div className="Title">
+                            Title 1
+                        </div>
+                    </SwiperSlide>);
+        })
+    
+        return slides;
+    };
 
     return (
         <div>
-            <Swiper {... params}>
-                <SwiperSlide><p className="text-center">Slide 1</p></SwiperSlide>
-                <SwiperSlide><p className="text-center">Slide 2</p></SwiperSlide>
-                <SwiperSlide><p className="text-center">Slide 3</p></SwiperSlide>
-                <SwiperSlide><p className="text-center">Slide 4</p></SwiperSlide>
+            <Swiper
+                navigation
+                className = "carousel-swiper"
+            >
+                <SwiperSlide><div className="title center-text">Title 1</div> </SwiperSlide>
             </Swiper>
         </div>
     )
--- src/Components/main/Main.js
+++ src/Components/main/Main.js
@@ -45,54 +45,7 @@ class Main extends Component {
     <div>
         <Carousel2 />
 
-        {/* <div className="container text-center my-3">
-            <h2 className="font-weight-light">Bootstrap 4 - Multi Item Carousel</h2>
-            <div className="row mx-auto my-auto">
-                <div id="recipeCarousel" className="carousel slide w-100" data-ride="carousel">
-                <a className="carousel-control-prev w-auto"  role="button" data-slide="prev">
-                    <span className="carousel-control-prev-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
-                    <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next w-auto" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon bg-dark border border-dark rounded-circle" aria-hidden="true"></span>
-                    <span className="sr-only">Next</span>
-                </a> 
-                    <div className="carousel-inner w-100" role="listbox">
-                        <div className="carousel-item active">
-                            <div className="col-md-4">
-                                <div className="card card-body">
-                                    <img className="img-fluid" src="http://placehold.it/380?text=1" alt='text1'/>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div className="col-md-4">
-                                <div className="card card-body">
-                                    <img className="img-fluid" src="http://placehold.it/380?text=2" alt='text2' />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div className="col-md-4">
-                                <div className="card card-body">
-                                    <img className="img-fluid" src="http://placehold.it/380?text=3" alt='text3' />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div className="col-md-4">
-                                <div className="card card-body">
-                                    <img className="img-fluid" src="http://placehold.it/380?text=4" alt='text4' />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> */}
-
         <InfoBox2 />
-       
         
         <div id="" className="container-fluid">
             <div data-section-id="video" data-section-type="video-section" className="mb30  video-section-video" data-videotype="youtube" data-videodisplay="autoplay_banner">
